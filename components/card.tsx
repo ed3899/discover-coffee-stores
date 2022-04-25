05c4cc61b33332bba5827b277e44bc5b542c4075
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-25 12:33:25 -0500

    card component styling - l67

--- components/card.tsx
+++ components/card.tsx
@@ -1,8 +1,14 @@
+//% libs
+import cls from "classnames";
+
 //% components
 
 // native
 import Link from "next/link";
 import Image from "next/image";
+
+//% styles
+import styles from "./card.module.css";
 
 type CardPropsT = {
   href: string;
@@ -10,12 +16,24 @@ type CardPropsT = {
   imgUrl: string;
 };
 
-const Card = (props:CardPropsT) => {
+const Card = (props: CardPropsT) => {
   return (
     <Link href={props.href}>
-      <a>
-        <h2>{props.name}</h2>
-        <Image src={props.imgUrl} width={260} height={160} />
+      <a className={styles.cardLink}>
+        <div className={cls("glass", styles.container)}>
+          <div className={styles.cardHeaderWrapper}>
+            <h2 className={styles.cardHeader}>{props.name}</h2>
+          </div>
+
+          <div className={styles.cardImageWrapper}>
+            <Image
+              className={styles.cardImage}
+              src={props.imgUrl}
+              width={260}
+              height={160}
+            />
+          </div>
+        </div>
       </a>
     </Link>
   );
