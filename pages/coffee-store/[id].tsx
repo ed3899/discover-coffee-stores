d605b152f627bbcde664b69b8234bce72e46f417
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-25 10:12:58 -0500

    add route using link component

--- pages/coffee-store/[id].tsx
+++ pages/coffee-store/[id].tsx
@@ -1,8 +1,19 @@
+//% libs
 import {useRouter} from "next/router";
+
+// native components
+import Link from "next/link";
 
 const CoffeeStore = () => {
   const router = useRouter();
-  return <div>Coffee Store Page {router.query.id}</div>;
+  return (
+    <div>
+      Coffee Store Page {router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+    </div>
+  );
 };
 
 export default CoffeeStore;
