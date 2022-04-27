fb02f2eec6a9b72f157760db7cb4eead9079ad9c
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-27 08:24:00 -0500

    created store reducer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,42 @@
 //% libs
-import {createContext, PropsWithChildren} from "react";
+import {createContext, PropsWithChildren, useReducer} from "react";
 // types
 import type {AppProps} from "next/app";
 
 //% styles
 import "../styles/globals.css";
 
+const ACTION_TYPES = {
+  SET_LAT_LONG: "SET_LAT_LONG",
+  SET_COFFEE_STORES: "SET_COFFEE_STORES",
+};
+
+const storeReducer = (state: any, action: {type: string; payload: any}) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return {...state, latLong: action.payload.latLong};
+    }
+
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return {...state, coffeeStores: action.payload.coffeeStores};
+    }
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
 const StoreContext = createContext({});
 
 const StoreProvider = ({children}: PropsWithChildren<{}>) => {
-  const initialState = {
+  const initialState: any = {
     latLong: "",
     coffeeStores: [],
   };
+
+  const [state, dispatch] = useReducer(storeReducer, initialState);
   return (
-    <StoreContext.Provider value={initialState}>
+    <StoreContext.Provider value={{state, dispatch}}>
       {children}
     </StoreContext.Provider>
   );
