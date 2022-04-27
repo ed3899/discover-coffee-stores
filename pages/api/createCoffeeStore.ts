7221abef0d2f6cd921718804d4eda6e6b8da2053
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-27 15:52:40 -0500

    transform coffee store data

--- pages/api/createCoffeeStore.ts
+++ pages/api/createCoffeeStore.ts
@@ -3,7 +3,7 @@
 // native
 import type {NextApiRequest, NextApiResponse} from "next";
 // external
-import Airtable from "airtable";
+import Airtable, {Record} from "airtable";
 
 const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(
   process.env.AIRTABLE_BASE_KEY!
@@ -13,21 +13,32 @@ const table = base("coffee-stores");
 
 const createCoffeeStore = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    // find record
-    const findCoffeeStoreRecords = await table
-      .select({
-        filterByFormula: `id="0"`,
-      })
-      .firstPage();
+    try {
+      // find record
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="0"`,
+        })
+        .firstPage();
 
-    console.group("createCoffeeStoreApi");
-    console.log({findCoffeeStoreRecords});
+      console.group("createCoffeeStoreApi");
+      console.log({findCoffeeStoreRecords});
 
-    if (findCoffeeStoreRecords.length !== 0) {
-      res.json(findCoffeeStoreRecords);
-    } else {
-      // create record
-      res.json({message: "create record"});
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = findCoffeeStoreRecords.map(record => {
+          return {
+            ...record.fields,
+          };
+        });
+        res.json(records);
+      } else {
+        // create record
+        res.json({message: "create record"});
+      }
+    } catch (error) {
+      console.error(`Error finding  store ${error}`);
+      res.status(500);
+      res.json({message: `Error finding store ${error}`});
     }
   }
 };
