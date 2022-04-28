8ea89a809044a068bff5ee8001561aa2f497bc81
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-28 08:02:18 -0500

    api error handling

--- pages/api/createCoffeeStore.ts
+++ pages/api/createCoffeeStore.ts
@@ -31,52 +31,64 @@ const createCoffeeStore = async (
       req.body as AirtableData;
     try {
       // find record
-      const findCoffeeStoreRecords = await table
-        .select({
-          filterByFormula: `id="${id}"`,
-        })
-        .firstPage();
 
-      console.group("createCoffeeStoreApi");
-      console.log({findCoffeeStoreRecords});
+      if (id) {
+        const findCoffeeStoreRecords = await table
+          .select({
+            filterByFormula: `id="${id}"`,
+          })
+          .firstPage();
 
-      if (findCoffeeStoreRecords.length !== 0) {
-        const records = findCoffeeStoreRecords.map(record => {
-          return {
-            ...record.fields,
-          };
-        }) as AirtableData[];
+        console.group("createCoffeeStoreApi");
+        console.log({findCoffeeStoreRecords});
 
-        res.json(records);
+        if (findCoffeeStoreRecords.length !== 0) {
+          const records = findCoffeeStoreRecords.map(record => {
+            return {
+              ...record.fields,
+            };
+          }) as AirtableData[];
+
+          res.json(records);
+        } else {
+          // create record
+
+          if (name) {
+            const createRecord = await table.create([
+              {
+                fields: {
+                  id,
+                  name,
+                  address,
+                  neighbourhood,
+                  vote,
+                  imgUrl,
+                },
+              },
+            ]);
+
+            const records = createRecord.map(record => {
+              return {
+                ...record.fields,
+              };
+            }) as AirtableData[];
+
+            console.groupEnd();
+
+            res.json({records});
+          } else {
+            res.status(400);
+            res.json({message: "Name is missing"});
+          }
+        }
       } else {
-        // create record
-        const createRecord = await table.create([
-          {
-            fields: {
-              id,
-              name,
-              address,
-              neighbourhood,
-              vote,
-              imgUrl,
-            },
-          },
-        ]);
-
-        const records = createRecord.map(record => {
-          return {
-            ...record.fields,
-          };
-        }) as AirtableData[];
-
-        console.groupEnd();
-
-        res.json({records});
+        res.status(400);
+        res.json({message: "Id missing"});
       }
     } catch (error) {
-      console.error(`Error finding  store ${error}`);
+      console.error(`Error creating or finding store ${error}`);
       res.status(500);
-      res.json({message: `Error finding store ${error}`});
+      res.json({message: `Error creating or finding store ${error}`});
     }
   }
 };
