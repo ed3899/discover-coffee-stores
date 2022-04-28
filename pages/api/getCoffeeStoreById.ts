4942c8ad079abf7521f5fc2c9e92519ceeca38f0
Eduardo Casanova <ed.wacc1995@gmail.com>
2022-04-28 10:52:21 -0500

    coffee store by id api validation - 151

--- pages/api/getCoffeeStoreById.ts
+++ pages/api/getCoffeeStoreById.ts
@@ -1,14 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 //% libs
-// native
+import table, {getMinifiedRecords} from "../../lib/airtable";
+// types
 import type {NextApiRequest, NextApiResponse} from "next";
 
-const getCoffeeStoreById = (req: NextApiRequest, res: NextApiResponse) => {
+const getCoffeeStoreById = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
   const {id} = req.query;
 
   try {
     if (id) {
-      res.json({message: `id is created ${id}`});
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="${id}"`,
+        })
+        .firstPage();
+
+      // console.group("createCoffeeStoreApi");
+      // console.log({findCoffeeStoreRecords});
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = getMinifiedRecords(findCoffeeStoreRecords);
+
+        res.json(records);
+      } else {
+        res.json({message: `Id could not be found`});
+      }
     } else {
       res.status(400);
       res.json({message: "Id is missing"});
